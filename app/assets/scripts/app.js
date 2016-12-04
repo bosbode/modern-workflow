6423b4a4c802605c04d1ee6920103109ce2d235f
Bosse Kemper <bosbode@gmail.com>
2016-12-04 15:36:03 +0100

    Finished first part of the lesson.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -5,5 +5,5 @@ import $ from "jquery";
 
 var mobileMenu = new MobileMenu;
 new RevealOnScroll($(".feature-item"), "85%");
-new RevealOnScroll($(".testimonial"), "60%");
+new RevealOnScroll($(".testimonial"), "70%");
 var stickyHeader = new StickyHeader;
