d9831d5278e70d0cbb3a856169a8cf9325a718e9
Bosse Kemper <bosbode@gmail.com>
2016-12-07 22:48:12 +0100

    Completed gulp build task.

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -30,7 +30,7 @@ var config = {
 };
 
 gulp.task("beginClean", function(){
-	return del(["./app/temp/sprite/", "./app/assets/images/icons/sprite"]);
+	return del(["./app/temp/sprite/", "./app/assets/images/sprite"]);
 });
 
 gulp.task("createSprite", ["beginClean"], function(){
@@ -47,7 +47,7 @@ gulp.task("createPngCopy", ["createSprite"], function(){
 
 gulp.task("copySpriteGraphic", ["createPngCopy"] ,function(){
 	return gulp.src("./app/temp/sprite/css/**/*.{svg,png}")
-	.pipe(gulp.dest("./app/assets/images/icons/sprite"));
+	.pipe(gulp.dest("./app/assets/images/sprite"));
 });
 
 gulp.task("copySpriteCSS", ["createSprite"], function(){
