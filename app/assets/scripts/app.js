2d43061fa57017fe8d70832f8cd9401b76671a43
Bosse Kemper <bosse@studiodumbar.com>
2016-12-07 13:35:06 +0100

    Add lazyload functionality.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -4,7 +4,6 @@ import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 import Modal from "./modules/Modal";
 
-
 var mobileMenu = new MobileMenu;
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "70%");
